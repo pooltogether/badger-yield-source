40c49b5a54b1192b1f8918b8d569ffee01ef4e94
steffel <2143646+steffenix@users.noreply.github.com>
2021-03-25 20:28:57 +0100

    use named accounts for args

--- deploy/00_deploy.ts
+++ deploy/00_deploy.ts
@@ -4,13 +4,12 @@ import {DeployFunction} from "hardhat-deploy/types";
 const func: DeployFunction =  async (hre: HardhatRuntimeEnvironment) => {
   const {deployments, getNamedAccounts} = hre;
   const {deploy} = deployments;
-  const {deployer} = await getNamedAccounts();
+  const {deployer, sushiBar, sushiToken} = await getNamedAccounts();
 
   console.time("SushiYieldSource deployed");
   const contract = await deploy('SushiYieldSource', {
     from: deployer,
-    args: ["0x8798249c2E607446EfB7Ad49eC89dD1865Ff4272",
-           "0x6B3595068778DD592e39A122f4f5a5cF09C90fE2"],
+    args: [sushiBar, sushiToken],
     log: true,
   });
 
