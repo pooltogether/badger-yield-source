ea1e5ce9b173225e69675533879ead4c4c88f229
steffel <2143646+steffenix@users.noreply.github.com>
2021-04-01 17:00:38 +0200

    update Natspec

--- contracts/SushiYieldSource.sol
+++ contracts/SushiYieldSource.sol
@@ -6,7 +6,8 @@ import { IYieldSource } from "@pooltogether/yield-source-interface/contracts/IYi
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "./ISushiBar.sol";
 import "./ISushi.sol";
-
+/// @title An pooltogether yield source for sushi token
+/// @author Steffel Fenix
 contract SushiYieldSource is IYieldSource {
     using SafeMath for uint256;
     address public sushiBar;
@@ -17,13 +18,13 @@ contract SushiYieldSource is IYieldSource {
         sushiBar = _sushiBar;
         sushiAddr = _sushiAddr;
     }
-
+    /// @title Token used for deposit
     /// @notice Returns the ERC20 asset token used for deposits.
     /// @return The ERC20 asset token
     function depositToken() public view override returns (address) {
         return (sushiAddr);
     }
-
+    /// @title balance of underlying token
     /// @notice Returns the total balance (in asset tokens).  This includes the deposits and interest.
     /// @return The underlying balance of asset tokens
     function balanceOfToken(address addr) public override returns (uint256) {
@@ -41,8 +42,8 @@ contract SushiYieldSource is IYieldSource {
 
         return (balances[addr].mul(sushiBalance).div(sourceShares));
     }
-
-    /// @notice Supplies tokens to the yield source.  Allows assets to be supplied on other user's behalf using the `to` param.
+    /// @title Supplies tokens to the yield source
+    /// @notice Allows assets to be supplied on other user's behalf using the `to` param.
     /// @param amount The amount of `token()` to be supplied
     /// @param to The user whose balance will receive the tokens
     function supplyTokenTo(uint256 amount, address to) public override {
@@ -56,8 +57,8 @@ contract SushiYieldSource is IYieldSource {
         uint256 balanceDiff = afterBalance.sub(beforeBalance);
         balances[to] = balances[to].add(balanceDiff);
     }
-
-    /// @notice Redeems tokens from the yield source.
+    /// @title Redeems tokens from the yield source.
+    /// @notice Redeems tokens from the yield source from the msg.sender, it burn yield bearing tokens and return token to the sender.
     /// @param amount The amount of `token()` to withdraw.  Denominated in `token()` as above.
     /// @return The actual amount of tokens that were redeemed.
     function redeemToken(uint256 amount) public override returns (uint256) {
