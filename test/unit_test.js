d2e9638b5f55497e774799feff6091712d0d1847
steffel <2143646+steffenix@users.noreply.github.com>
2021-04-03 10:54:39 +0200

    use multiple variations of amount

--- test/unit_test.js
+++ test/unit_test.js
@@ -4,6 +4,7 @@ const chai = require("chai");
 
 chai.use(solidity);
 const toWei = ethers.utils.parseEther;
+const toEth = ethers.utils.formatEther;
 const { expect } = chai;
 
 let overrides = { gasLimit: 9500000 };
@@ -82,18 +83,34 @@ describe("SushiYieldSource", function () {
     expect(await sushi.balanceOf(wallet.address)).to.eq(amount);
   });
 
-  it("deposit, sushi accrues, withdrawal", async function () {
-    await sushi.connect(wallet).approve(yieldSource.address, amount);
-    await yieldSource.supplyTokenTo(amount, wallet.address);
-    // increase total balance by 1%
-    await sushi.mint(sushiBar.address, amount);
-    expect(await yieldSource.callStatic.balanceOfToken(wallet.address)).to.eq(
-      amount.mul(101).div(100)
-    );
+  [toWei("100"), toWei("100").mul(10), toWei("100").mul(99)].forEach(function (
+    amountToDeposit
+  ) {
+    it(
+      "deposit " + toEth(amountToDeposit) + ", sushi accrues, withdrawal",
+      async function () {
+        await sushi.mint(wallet.address, amountToDeposit.sub(amount));
+        await sushi
+          .connect(wallet)
+          .approve(yieldSource.address, amountToDeposit);
+        await yieldSource.supplyTokenTo(amountToDeposit, wallet.address);
+        // increase total balance by amount
+        await sushi.mint(sushiBar.address, amount);
 
-    await yieldSource.redeemToken(amount.mul(101).div(100));
-    expect(await sushi.balanceOf(wallet.address)).to.eq(
-      amount.mul(101).div(100)
+        const totalAmount = await yieldSource.callStatic.balanceOfToken(
+          wallet.address
+        );
+        const expectedAmount = amountToDeposit
+          .mul(amountToDeposit.add(amount.mul(100)))
+          .div(amountToDeposit.add(amount.mul(99)));
+        expect(totalAmount).to.eq(expectedAmount);
+
+        await yieldSource.redeemToken(totalAmount);
+        expect(await sushi.balanceOf(wallet.address)).to.be.closeTo(
+          totalAmount,
+          1
+        );
+      }
     );
   });
 });
