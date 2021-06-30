3d4ba822da9ee4db4b20d2e9672561f3fc66335a
Aodhgan <aodhgan@pooltogether.com>
2021-06-29 17:54:23 -0700

    added events

--- contracts/BadgerYieldSource.sol
+++ contracts/BadgerYieldSource.sol
@@ -16,6 +16,21 @@ contract BadgerYieldSource is IYieldSource {
     IBadger private immutable badger;
     mapping(address => uint256) private balances;
 
+    /// @notice Emitted when asset tokens are redeemed from the yield source
+    event RedeemedToken(
+        address indexed from,
+        uint256 shares,
+        uint256 amount
+    );
+
+    /// @notice Emitted when asset tokens are supplied to the yield source
+    event SuppliedTokenTo(
+        address indexed from,
+        uint256 shares,
+        uint256 amount,
+        address indexed to
+    );
+
     constructor(address badgerSettAddr, address badgerAddr) public {
         badgerSett = IBadgerSett(badgerSettAddr);
         badger = IBadger(badgerAddr);
@@ -49,6 +64,8 @@ contract BadgerYieldSource is IYieldSource {
         uint256 afterBalance = badgerSett.balanceOf(address(this));
         uint256 balanceDiff = afterBalance.sub(beforeBalance);
         balances[to] = balances[to].add(balanceDiff);
+        
+        emit SuppliedTokenTo(msg.sender, balanceDiff, amount, to);
     }
 
     /// @notice Redeems tokens from the yield source to the msg.sender, it burns yield bearing tokens and returns token to the sender.
@@ -76,7 +93,10 @@ contract BadgerYieldSource is IYieldSource {
         uint256 badgerBalanceDiff = badgerAfterBalance.sub(badgerBeforeBalance);
 
         balances[msg.sender] = balances[msg.sender].sub(requiredSharesBalance);
+
         badger.transfer(msg.sender, badgerBalanceDiff);
+        emit RedeemedToken(msg.sender, requiredSharesBalance, amount);
+
         return (badgerBalanceDiff);
     }
 }
